68b9e8bc41ef2293a35d534fbbcea28886b5c509
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2025-02-10 17:43:32 -0300

    probando angular 19

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeroItemComponent } from './components/hero-item/hero-item.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeroItemComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'prueba';
+}
---
+++ src/app/components/hero-item/hero-item.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Hero } from '../../shared/hero.interface';
+
+@Component({
+  selector: 'app-hero-item',
+  imports: [],
+  templateUrl: './hero-item.component.html',
+  styleUrl: './hero-item.component.css'
+})
+export class HeroItemComponent {
+  hero: Hero ={
+    id:5,
+    name: "Aioria de Leo",
+    powerstats:{
+      intelligence: 75,
+      strength: 100,
+      speed: 100,
+      durability: 90,
+      power: 100,
+      combat: 85
+    },
+    image:'https://imgs.search.brave.com/_UG6TRXd6rpSlLTuaqU9XWW_cU4gey6Sur1RfiyVZ6w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzFlLzdh/LzU4LzFlN2E1OGFl/NjRkY2I1ZDU0ZjQz/NGQ3MzgyNDBkYTZk/LmpwZw',
+    alignment: 'good',
+
+  }
+
+}
